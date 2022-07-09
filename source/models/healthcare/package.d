ac004da61fed49a15e575ccf15e78319397b5bbe
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-07-09 12:41:38 +0200

    Update package.d

--- source/models/healthcare/package.d
+++ source/models/healthcare/package.d
@@ -1,1 +1,7 @@
 module models.healthcare;
+
+public import models.healthcare.attributes;
+public import models.healthcare.classes;
+public import models.healthcare.elements;
+public import models.healthcare.entities;
+public import models.healthcare.tests;
